cf66f48ceaed9b162e3bc21c3f783c8a32982076
shukannnn <shukanpanchal411@gmail.com>
2026-07-10 21:04:14 +0530

    changes in path of isolate:

--- internal/worker/code_execution_worker.go
+++ internal/worker/code_execution_worker.go
@@ -74,7 +74,7 @@ func (worker CodeExecutionWorker) Process(ctx context.Context, job *jobs.Job) er
 
 	
 	// copying the source code into isoltate
-	solutionFileName := fmt.Sprintf("/var/local/lib/isolate/%d/box/", boxID)
+	solutionFileName := fmt.Sprintf("/var/lib/isolate/%d/box/", boxID)
 
 	if payload.Language == "python3" {
 		solutionFileName = solutionFileName + "solution.py"
@@ -86,7 +86,7 @@ func (worker CodeExecutionWorker) Process(ctx context.Context, job *jobs.Job) er
 
 	// copying the stdin into isolate, if stdin is present
 	if payload.Stdin != "" {
-		stdinFileName := fmt.Sprintf("/var/local/lib/isolate/%d/box/stdin.txt", boxID)
+		stdinFileName := fmt.Sprintf("/var/lib/isolate/%d/box/stdin.txt", boxID)
 		if err := os.WriteFile(stdinFileName, []byte(payload.Stdin), 0644); err != nil {
 			return fmt.Errorf("error while writing stdin file to isolate for code execution worker: %w", err) 
 		}
@@ -120,8 +120,8 @@ func (worker CodeExecutionWorker) Process(ctx context.Context, job *jobs.Job) er
 	}
 
 	// reading the meta, stderr and stdout output from the process
-	stdoutFileName := fmt.Sprintf("/var/local/lib/isolate/%d/box/stdout.txt", boxID)
-	stderrFileName := fmt.Sprintf("/var/local/lib/isolate/%d/box/stderr.txt", boxID)
+	stdoutFileName := fmt.Sprintf("/var/lib/isolate/%d/box/stdout.txt", boxID)
+	stderrFileName := fmt.Sprintf("/var/lib/isolate/%d/box/stderr.txt", boxID)
 	metaFileName := fmt.Sprintf("/tmp/meta-%d.txt", boxID)
 
 	stdoutContent, err := os.ReadFile(stdoutFileName)
